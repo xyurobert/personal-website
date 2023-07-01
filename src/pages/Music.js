776a43c30133a7d430ed444be9dcadf76ee1f0ed
xyurobert <xyu.robert@gmail.com>
2023-06-30 23:47:39 -0500

    gh-pages changes

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/style.scss';
+import styles from '../styles/skeleton.scss';
 import Sidebar from '../components/Sidebar';
 
 function Music() {
