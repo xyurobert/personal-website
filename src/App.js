9fe5864d9c8cd297f8b9ed08fc5b37aa0b916d4d
xyurobert <xyu.robert@gmail.com>
2023-07-02 08:56:56 -0500

    include hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter, Navigate } from 'react-router-dom';
 import './App.scss';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -9,16 +9,17 @@ import Music from './pages/Music';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <Routes>
           <Route exact path="/" element={<About/>} />
           <Route exact path="/projects" element={<Projects/>} />
           <Route exact path="/reading" element = {<Reading/>} />
           <Route exact path="/music" element = {<Music/>} />
+          <Route path="/*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
