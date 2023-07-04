781a76b5266c8be3af89b517b2a74560dabe69a5
xyurobert <xyu.robert@gmail.com>
2023-07-03 19:10:12 -0500

    added piano section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, HashRouter, Navigate } from 'react-router-dom';
 import './App.scss';
 import About from './pages/About';
+import { Helmet } from 'react-helmet';
+
 
 function App() {
   return (
     <HashRouter>
+      <Helmet>
+        <title>Robert Xu</title>
+        <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/4311/4311197.png"/>
+      </Helmet>
       <div className="App">
         <Routes>
           <Route exact path="/" element={<About/>} />
--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,7 @@ function About() {
   const handleScrollToAbout = () => {
     let yOffset = -20;
     if(window.innerWidth < 900){
-      yOffset = -160;
+      yOffset = -180;
     }
     console.log(yOffset)
     const element = aboutRef.current;
@@ -150,7 +150,79 @@ function About() {
 
         <div className='music-container' ref={musicRef}>
           <h2>Piano</h2>
-          <p>[work in progress]</p>
+          <div className='pianos-container'>
+          <div>Currently working on: </div>
+          <div className='piano-current'>
+            <div>Chopin - Ballade no. 1 in G minor</div>
+            <div>Debussy - Clair de lune</div>
+          </div>
+          
+          <a className='link' target="_blank" rel="noopener noreferrer" href="https://youtu.be/HgnVzt5APOI">
+              <div className='piano-container'>
+                  <div className='project-title'>
+                    Liszt - Un Sospiro <FaArrowRight className='arrow'/>
+                  </div>
+                  <div className='piano-info'>
+                    <img className='thumbnail' src='https://i9.ytimg.com/vi_webp/HgnVzt5APOI/mq2.webp?sqp=CJyojaUG-oaymwEmCMACELQB8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgVShEMA8=&rs=AOn4CLDY-bERUzczNytCi1oW5_ZIxBYs_Q'></img>
+                    <div className='piano-year'>
+                      2021
+                    </div>
+                  </div>
+              </div>
+          </a> 
+          <a className='link' target="_blank" rel="noopener noreferrer" href="https://youtu.be/tRmv90OdB6k">
+              <div className='piano-container'>
+                  <div className='project-title'>
+                    Debussy - Arabesque No. 1 <FaArrowRight className='arrow'/>
+                  </div>
+                  <div className='piano-info'>
+                    <img className='thumbnail' src='https://i9.ytimg.com/vi_webp/tRmv90OdB6k/mq2.webp?sqp=CPCljaUG-oaymwEmCMACELQB8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWyhOMA8=&rs=AOn4CLDl0gXPCpd6EmHU3wDxvb_ANVJZGw'></img>
+                    <div className='piano-year'>
+                      2021
+                    </div>
+                  </div>
+              </div>
+          </a> 
+          <a className='link' target="_blank" rel="noopener noreferrer" href="https://youtu.be/awX8JkBLByc">
+              <div className='piano-container'>
+                  <div className='project-title'>
+                    Mozart - Sonata No. 9 in D, K. 311 First Movement <FaArrowRight className='arrow'/>
+                  </div>
+                  <div className='piano-info'>
+                    <img className='thumbnail' src='https://i9.ytimg.com/vi_webp/awX8JkBLByc/mq2.webp?sqp=CJyojaUG-oaymwEmCMACELQB8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgXShPMA8=&rs=AOn4CLBppZZ2PB9ty_UxWiAQ9G3L6gTJNw'></img>
+                    <div className='piano-year'>
+                      2021
+                    </div>
+                  </div>
+              </div>
+          </a> 
+          <a className='link' target="_blank" rel="noopener noreferrer" href="https://youtu.be/gFTWEcGNcw4">
+              <div className='piano-container'>
+                  <div className='project-title'>
+                    Du - The Dance of Watergrass <FaArrowRight className='arrow'/>
+                  </div>
+                  <div className='piano-info'>
+                    <img className='thumbnail' src='https://i.ytimg.com/vi/gFTWEcGNcw4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC2FGoT1Rf0oNmu_u7SkmdoNqG26w'></img>
+                    <div className='piano-year'>
+                      2020
+                    </div>
+                  </div>
+              </div>
+          </a> 
+          <a className='link' target="_blank" rel="noopener noreferrer" href="https://youtu.be/VHqxtYL111M">
+              <div className='piano-container'>
+                  <div className='project-title'>
+                    Agay - Little Rhapsody on Hungarian Themes <FaArrowRight className='arrow'/>
+                  </div>
+                  <div className='piano-info'>
+                    <img className='thumbnail' src='https://i.ytimg.com/vi/VHqxtYL111M/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLABjEKUFHcHAbrak4JA2KN8-_K1KA'></img>
+                    <div className='piano-year'>
+                      2015
+                    </div>
+                  </div>
+              </div>
+            </a>         
+          </div>
         </div>
       </div>
     </div>
